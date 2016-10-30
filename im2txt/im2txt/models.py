8a48c9b54cbab708846aa87e87e6e247a416ea20
Vikesh Khanna <khanna.vikesh@gmail.com>
2016-10-30 00:31:31 -0700

    Updating im2txt

--- im2txt/im2txt/models.py
+++ im2txt/im2txt/models.py
@@ -11,33 +11,55 @@ import tensorflow as tf
 from ambient.serving.client import TFModel
 from ambient.serving.interface import CVShow, CVClient
 
+from ambient.search.metadata import EntityMetadata
+
+# HOST = 'localhost'
+HOST = 'ec2-54-244-39-243.us-west-2.compute.amazonaws.com'
+PORT = 9003
+
 
 class Im2TxtStubModel(TFModel):
     '''Create a stub TFModel to allow orchestrator to work
     '''
     # HIJACK init to not call at()
-    def __init__(self, host, port):
-        self.client = CVClient(host, port)
+    def __init__(self):
         self.frame_num = 0
+
+    def at(self, host, port):
+        self.client = CVClient(host, port)
+        return self
 
     def get(self, frame):
         '''Model object is the most likely sentence
         '''
         self.frame_num += 1
         # HAX: Encode frame
-        filename = '/tmp/im2txt/frame' + str(self.frame_num % 100) + '.jpg'
-        rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # filename = '/tmp/im2txt/frame' + str(self.frame_num % 5000) + '.jpg'
         # Hack: Encode frame
-        cv2.imwrite(filename, frame)
+        # cv2.imwrite(filename, frame)
         
         # Read encoded file
-        with tf.gfile.GFile(filename, "r") as f:
-            image = f.read()
+        # with tf.gfile.GFile(filename, "r") as f:
+        #     image = f.read()
         
-        return self.client.get(image)
-
-    def set(self, sentence, frame):
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90] 
+        result, image = cv2.imencode('.jpg', frame, encode_param)
+        bstr = image.tostring()
+        print("Len: {} bytes".format(len(bstr)))
+        assert result, 'Could not encode'
+        sentence = self.client.get(bstr)
         height, width = frame.shape[:2]
-        cv2.rectangle(frame, (0, height - 100), (width, height), (153, 122, 0), -1)
-        cv2.putText(frame, sentence.title(), (5, height-75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+        return [EntityMetadata('scene', (0, height - 50, width, 50), 
+            keywords=sentence.title().split(), custom=sentence)]
+
+    def set(self, entities, frame):
+        height, width = frame.shape[:2]
+        # There's only one entity for im2txt
+        sentence = entities[0].custom
+        x, y, w, h = entities[0].roi
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (153, 122, 0), -1)
+        cv2.putText(frame, sentence.title(), (5, height-25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
         return frame
+
+def get_model():
+    return Im2TxtStubModel().at(HOST, PORT)
